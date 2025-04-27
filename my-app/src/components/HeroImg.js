15847915176cc188ae2c6ba64d79531d61de9ce9
pidepiper009 <88837112+pidepiper009@users.noreply.github.com>
2025-04-27 16:39:23 +1000

    created hero component and added some assets

---
+++ my-app/src/components/HeroImg.js
@@ -0,0 +1,29 @@
+import "./HeroImgStyles.css";
+
+import React from "react";
+import IntroImg from "../assets/intro-bg.jpg";
+import { Link } from "react-router-dom";
+
+const HeroImg = () => {
+  return (
+    <div className="hero">
+      <div className="mask">
+        <img className="intro-img" src={IntroImg} alt="IntroImg" />
+      </div>
+      <div className="content">
+        <p>HI, I'M XIAN HU.</p>
+        <h1>Software Developer.</h1>
+        <div>
+          <Link to="/" className="btn">
+            Projects
+          </Link>
+          <Link to="/contact" className="btn btn-light">
+            Contact
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroImg;
