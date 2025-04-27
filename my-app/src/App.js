99531a548904446f0f8cd19e96503a5e02557a4b
pidepiper009 <88837112+pidepiper009@users.noreply.github.com>
2025-04-27 15:11:06 +1000

    created all pages and defined routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./index.css";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
