79be883165cbcc66dd0742b0424fabd701ae5002
pidepiper009 <88837112+pidepiper009@users.noreply.github.com>
2025-04-27 15:27:39 +1000

    created navbar component

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import "./NavbarStyles.css";
+
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="header">
+      <Link to="/">
+        <h1>Portfolio</h1>
+      </Link>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
