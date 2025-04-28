935ee8fe3068e3ca0b712522937ba91d631478a3
pidepiper009 <88837112+pidepiper009@users.noreply.github.com>
2025-04-28 16:19:26 +1000

    completed footer component

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,74 @@
+import "./FooterStyles.css";
+
+import React from "react";
+
+import {
+  FaFacebook,
+  FaHome,
+  FaLinkedin,
+  FaMailBulk,
+  FaPhone,
+  FaTwitter,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left">
+          <div className="location">
+            <h4>
+              <FaHome
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              Melbourne, VIC 3000
+            </h4>
+          </div>
+          <div className="phone">
+            <h4>
+              <FaPhone
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              0434423665
+            </h4>
+          </div>
+          <div className="email">
+            <h4>
+              <FaMailBulk
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              huxian126@gmail.com
+            </h4>
+          </div>
+        </div>
+
+        <div className="right">
+          <h4>About the company</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <div className="social">
+            <FaFacebook
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+            <FaTwitter
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+            <FaLinkedin
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
