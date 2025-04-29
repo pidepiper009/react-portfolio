30c277178d8ecef0ab05dee9b9e3d35619127a92
pidepiper009 <88837112+pidepiper009@users.noreply.github.com>
2025-04-29 15:04:34 +1000

    Added projects section to homepage

---
+++ my-app/src/components/WorkCardData.js
@@ -0,0 +1,26 @@
+import pro1 from "../assets/project1.png";
+import pro2 from "../assets/project2.png";
+import pro3 from "../assets/project3.png";
+
+const WorkCardData = [
+  {
+    imgsrc: pro1,
+    title: "Ecommerce Website",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    view: "#",
+  },
+  {
+    imgsrc: pro2,
+    title: "LMS/Course Website",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    view: "#",
+  },
+  {
+    imgsrc: pro3,
+    title: "Track and Monitor",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    view: "#",
+  },
+];
+
+export default WorkCardData;
